575b85aab5d832ecbf35f0ee5aef732057df3d40
Viduth04 <jmadheepa@gmail.com>
2026-01-19 02:53:52 +0530

    Allow Vercel frontend in CORS

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173', 
+    'https://imax-frontend-hygq92yyo-jmadheepa-1636s-projects.vercel.app'
+  ],
   credentials: true
 }));
 app.use(express.json());
